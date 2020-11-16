06eb38956c72d911cf2f4e8c595339124a09f295
Raj <rajkamal11126@gmail.com>
2020-11-17 02:14:58 +0530

    Refactor : Functionality to check the count of dice win

--- Utility.js
+++ Utility.js
@@ -9,19 +9,23 @@ diceRoll = () => {
     return rollDie;
 }
 startPlaying = () => {
+    let count = 0;
     while ( playerPosition < 100 ) {
         let option = Math.floor(Math.random() * 10) % 3;
         switch (option) {
             case NO_PLAY:
                 playerPosition = playerPosition;
+                count++ ;
             case LADDER:
                 if (( playerPosition + diceRoll()) <= 100 ) {
                     playerPosition = playerPosition + diceRoll()
                 };
+                count++ ;
             case SNAKE:
                 playerPosition = playerPosition - diceRoll();
+                count++ ;
         }
-        console.log(playerPosition);
+        console.log(count +" "+ playerPosition);
     }   
 }
 module.exports = { startPlaying };
